ededb1a26a113578ac6a2522072a27f3bd5ea799
Fikry <fikrysolot@gmail.com>
2020-04-30 11:11:31 +0700

    fixing nav

--- script2.js
+++ script2.js
@@ -19,6 +19,43 @@ submit.addEventListener('click', function(){
       )
 })
 
+// show my IP
+const tombolNav = document.querySelector('.tombol-nav');
+tombolNav.addEventListener('click', function(){
+  Swal.fire({
+    title: 'Submit your Github username',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Look up',
+    showLoaderOnConfirm: true,
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .catch(error => {
+          Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      })
+    }
+  })
+})
+
 // SCROLL TO TOP
 //Get the button
 var mybutton = document.getElementById("myBtn");
